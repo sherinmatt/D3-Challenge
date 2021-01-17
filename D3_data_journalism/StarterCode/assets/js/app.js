152dd7be9acf7f8d2bc9faf12bf715adc83a7510
Sherin M <sherinm@Sherins-MacBook-Pro.local>
2021-01-16 23:46:20 -0500

    updates

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -1,5 +1,5 @@
 // @TODO: YOUR CODE HERE!
-// healthcate vs poverty
+
 
 // Part 1
 
@@ -9,6 +9,8 @@
 // circle
 
 // area dimension
+
+// Healthcare vs Poverty
 
 var svgWidth = 900;
 var svgHeight = 500;
@@ -37,3 +39,28 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${chartsMargin.left}, ${chartsMargin.top})`);
 
 // getting data
+d3.csv("./assets/data/data.csv").then(function(usData) {
+  console.log(usData);
+
+  // parsing data
+  usData.forEach(function(data) {
+    data.healthcare = +data.healthcare;
+    data.poverty = +data.poverty;
+  });
+
+  //scale funct
+  var xLinearScale = d3.scaleLinear()
+      .domain([20, d3.max(usData, d => d.poverty)])
+      .range([0, width]);
+
+  var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(usData, d => d.healthcare)])
+      .range([height, 0]);
+  
+  // axis funct
+
+  var bAxis = d3.axisBottom(xLinearScale);
+  var lAxis = d3.axisLeft(yLinearScale);
+
+
+});
