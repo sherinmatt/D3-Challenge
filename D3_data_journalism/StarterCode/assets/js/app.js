537366295d64aa4d5a2906d2eb25a34ace5dfdf1
Sherin M <sherinm@Sherins-MacBook-Pro.local>
2021-01-16 21:07:56 -0500

    updates

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -1,1 +1,28 @@
 // @TODO: YOUR CODE HERE!
+// healthcate vs poverty
+
+// Part 1
+
+
+// create scatterplot whcih represents each state with circle elements. 
+// include state abbreviation in circles
+// circle
+
+// area dimension
+
+var svgWidth = 900;
+var svgHeight = 500;
+
+// chartsMargin
+var chartsMargin = {
+    top: 35,
+    right: 35,
+    bottom: 65,
+    left: 85
+};
+
+// making dimensions of chart area
+var chartWidth = svgWidth - chartsMargin.left - chartsMargin.right;
+var chartHeight = svgHeight - chartsMargin.top - chartsMargin.bottom;
+
+
